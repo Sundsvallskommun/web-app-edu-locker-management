02d96046f56dd6dd4880407e69de5d3d2df31f22
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2025-09-01 15:56:32 +0200

    feat: get classes from edu 2

--- backend/src/responses/school.response.ts
+++ backend/src/responses/school.response.ts
@@ -3,7 +3,7 @@ import { LockerBuilding } from '@/data-contracts/pupillocker/data-contracts';
 import ApiResponse from '@/interfaces/api-service.interface';
 import { IsNullable } from '@/utils/custom-validation-classes';
 import { Type } from 'class-transformer';
-import { IsOptional, IsString, ValidateNested } from 'class-validator';
+import { IsDateString, IsOptional, IsString, ValidateNested } from 'class-validator';
 
 export class SchoolUnit implements SchoolUnitV2 {
   @IsString()
@@ -34,6 +34,20 @@ export class Building implements LockerBuilding {
   @IsNullable()
   floors?: string[] | null;
 }
+export class SchoolGroup {
+  @IsString()
+  groupId: string;
+  @IsString()
+  displayName: string;
+  @IsDateString()
+  startDate: string;
+  @IsDateString()
+  endDate: string;
+  @IsString()
+  schoolUnitId: string;
+  @IsString()
+  groupType: string;
+}
 
 export class School implements SchoolWithUnits {
   @IsString()
@@ -54,6 +68,10 @@ export class School implements SchoolWithUnits {
   @ValidateNested({ each: true })
   @Type(() => Building)
   buildings?: Building[];
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => SchoolGroup)
+  groups?: SchoolGroup[];
 }
 
 export class SchoolApiResponse implements ApiResponse<School[]> {
